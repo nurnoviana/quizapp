d20123fe3d003eeb610773003b9a2501e5e46620
Noviana Nur Rizki <nurnoviana97@gmail.com>
2022-02-07 10:02:46 +0700

    first commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa kepanjangan dari HTML?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language",
+            "Hyper Text Mobile Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa kepanjangan dari CSS?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Cascading Style Sheet",
+            "Correctness Style Sheet",
+            "Cascading Style Skin",
+            "Cascading Spring Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Pada saat membuat halaman homepage, sebaiknya menggunakan nama...",
+        answer: "index.html",
+        options: [
+            "index.html",
+            "style.html",
+            "style.js",
+            "index.css"
+        ]
+    },
+    {
+        numb: 4,
+        question: "List yang dibuat di dalam list adalah...",
+        answer: "Nested list",
+        options: [
+            "Unordered list",
+            "Nested list",
+            "Ordered list",
+            "Description list"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Berikut ini merupakan aplikasi Teks Editor, kecuali...",
+        answer: "Blender",
+        options: [
+            "Notepad++",
+            "Sublime Text",
+            "Visual Studio Code",
+            "Blender"
+        ]
+    }
+]
